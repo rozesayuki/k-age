13c81d9fb7c0379cf23fac6d5060760e3529b696
sayukiyamada <sayukilife@gmail.com>
2022-05-22 11:02:44 +0900

    Initial Commit

---
+++ k age/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  k age
+//
+//  Created by SayukiYamada on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
